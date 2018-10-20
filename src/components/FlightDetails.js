dc909e73c7252259b2fcad3c9ed9870e2ef1f40b
Zachery Gentry <zacherygentry@gmail.com>
2018-10-20 16:58:02 -0500

    flight details card added with fake data

--- src/components/FlightDetails.js
+++ src/components/FlightDetails.js
@@ -3,24 +3,35 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 // Material UI
 import Card from '@material-ui/core/Card';
-import { CardContent } from '@material-ui/core';
+import { CardContent, Icon } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+import { Flight } from '@material-ui/icons';
 // CSS
 import '../App.css';
 
 
 const styles = {
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
     },
-    title: {
-        fontSize: 14,
+    headerTitle: {
+        marginLeft: '2%'
     },
-    pos: {
-        marginBottom: 12,
+    body: {
+        marginTop: '4%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
+    detail: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+
+    centerChild: {
+        margin: 'auto'
+    }
 };
 
 
@@ -30,12 +41,42 @@ function FlightDetails(props) {
     return (
         <Card className='Card'>
             <CardContent>
-                <Typography variant="h6" component="h5">
-                    Flight Details
-                </Typography>
+                <div className={classes.header}>
+                    <Icon> <Flight /> </Icon>
+                    <Typography component="h2" className={classes.headerTitle}>
+                        Flight Details
+                    </Typography>
+                </div>
+
+                <div className={classes.body}>
+                    <Detail title="Gate Number" content="A16"></Detail>
+                    <Detail title="Flight" content="219123"></Detail>
+                    <Detail title="Boarding Time" content="12:32pm"></Detail>
+                </div>
+
             </CardContent>
-        </Card>
+        </Card >
     );
+}
+
+class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div style={styles.detail}>
+                <Typography style={styles.centerChild} component='h2'>
+                    {this.props.title}
+                </Typography>
+                <Typography style={styles.centerChild} variant='h6' component='h2'>
+                {this.props.content}
+                </Typography>
+            </div>
+        );
+    }
 }
 
 FlightDetails.propTypes = {
