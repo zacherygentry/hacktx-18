6352d718c6ad3ff21442b82cfff22569e6fb000a
Zachery Gentry <zacherygentry@gmail.com>
2018-10-20 21:20:10 -0500

    changed theme

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import TrendingHashtags from './components/trendinghashtags';
 import FlightDetails from './components/FlightDetails';
 import ToDo from './components/ToDo';
 import Weather from './components/Weather';
 import RideServices from './components/RideServices';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0078d2'
+    },
+    secondary: {
+      main: '#4db4fa',
+    },
+  },
+});
 
 class SimpleAppBar extends React.Component {
   constructor(props) {
@@ -32,20 +45,23 @@ class SimpleAppBar extends React.Component {
 
   render() {
     return (
+      <MuiThemeProvider theme={theme}>
       <div className='Root'>
-        <AppBar position="static" color="default">
+        <AppBar color="secondary" position="static">
           <Toolbar>
             <Typography variant="h6" color="inherit">
               Welcome, {this.state.name}
             </Typography>
           </Toolbar>
         </AppBar>
+
         <FlightDetails></FlightDetails>
         <ToDo></ToDo>
         <Weather></Weather>
         <TrendingHashtags />
         <RideServices></RideServices>
-      </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -22,7 +22,6 @@ import '../App.css';
 const styles = theme => ({
     root: {
       width: '100%',
-      backgroundColor: theme.palette.background.paper,
     },
   });
 
