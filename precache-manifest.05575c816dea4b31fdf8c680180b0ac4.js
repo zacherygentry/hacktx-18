73028464148e16afca1f3de040bd07c2001a2648
Kenneth Cassel <kennethcassel@gmail.com>
2018-10-21 00:53:14 -0500

    Updates

---
+++ precache-manifest.05575c816dea4b31fdf8c680180b0ac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b59df42444001327fb",
+    "url": "/myplanepal/static/js/runtime~main.30b59df4.js"
+  },
+  {
+    "revision": "59af1ac81c2d0de4ef14",
+    "url": "/myplanepal/static/js/main.59af1ac8.chunk.js"
+  },
+  {
+    "revision": "8a17ffcb34fdec19bd9c",
+    "url": "/myplanepal/static/js/1.8a17ffcb.chunk.js"
+  },
+  {
+    "revision": "59af1ac81c2d0de4ef14",
+    "url": "/myplanepal/static/css/main.8e6641c5.chunk.css"
+  },
+  {
+    "revision": "da9856ce90efd5f923c3ea08498f7b2a",
+    "url": "/myplanepal/index.html"
+  }
+];
